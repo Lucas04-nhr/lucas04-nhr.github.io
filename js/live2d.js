0db5041788f35dc324e0efc05617779daeb5889f
Lucas <lucas04@foxmail.com>
2024-03-24 00:26:29 +0800

    Add Live2D widget to default.html

---
+++ js/live2d.js
@@ -0,0 +1,21 @@
+L2Dwidget.init({
+    "model": {
+        jsonPath: "https://unpkg.com/live2d-widget-model-tororo@1.0.5/assets/tororo.model.json",
+        "scale": 1
+    },
+    "display": {
+        "position": "left", //模型的表现位置
+        "width": 150, //模型的宽度
+        "height": 300, //模型的高度
+        "hOffset": 0,
+        "vOffset": -20
+    },
+    "mobile": {
+        "show": true,
+        "scale": 0.5
+    },
+    "react": {
+        "opacityDefault": 0.7, //模型默认透明度
+        "opacityOnHover": 0.2
+    }
+    });

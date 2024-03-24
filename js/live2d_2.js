e274e9a48c032b1e01a030395c2f19e538f37525
Lucas <lucas04@foxmail.com>
2024-03-24 11:21:58 +0800

    Add live2d_2.js script to default.html

--- js/live2d_2.js
+++ js/live2d_2.js
@@ -1,11 +1,11 @@
 L2Dwidget.init(
     {
         "model": {
-            jsonPath: "https://unpkg.com/live2d-widget-model-tororo@1.0.5/assets/tororo.model.json",
+            jsonPath: "https://unpkg.com/live2d-widget-model-wanko@1.0.5/assets/wanko.model.json",
             "scale": 1
         },
         "display": {
-            "position": "left", //模型的表现位置
+            "position": "right", //模型的表现位置
             "width": 150, //模型的宽度
             "height": 300, //模型的高度
             "hOffset": 50,

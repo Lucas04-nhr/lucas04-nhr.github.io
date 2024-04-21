f5721b1ee66bfb3d40e4eef207d5365e04080968
Lucas <lucas04@foxmail.com>
2024-04-21 22:03:54 +0800

    Update footer links and badges in _includes/footer.html

--- js/lottie.js
+++ js/lottie.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function() {
     // 获取Lottie动画元素
     var lottieAnimation = document.getElementById('lottie-animation');
     // 设置Lottie动画元素的样式
-    lottieAnimation.style.transition = 'opacity 1s ease-out';
+    lottieAnimation.style.transition = 'opacity 2.5s ease-out';
     // 隐藏Lottie动画
     lottieAnimation.style.display = 'none';
 });

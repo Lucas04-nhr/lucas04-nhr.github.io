8bd743ef057ab37412a1e9939ebe2e8a77581f46
Lucas <lucas04@foxmail.com>
2024-04-21 21:53:00 +0800

    Add Lottie animation to the website

---
+++ js/lottie.js
@@ -0,0 +1,7 @@
+// 监听页面加载事件
+window.addEventListener('load', function() {
+    // 获取Lottie动画元素
+    var lottieAnimation = document.getElementById('lottie-animation');
+    // 隐藏Lottie动画
+    lottieAnimation.style.display = 'none';
+});

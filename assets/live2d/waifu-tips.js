0bc03c1659f2749f8bfc59db054b2a13b3ff5f21
Lucas <lucas04@foxmail.com>
2024-05-25 23:27:56 +0800

    chore: Update waifu-tips.js and waifu.css for improved font and tool settings

--- assets/live2d/waifu-tips.js
+++ assets/live2d/waifu-tips.js
@@ -66,8 +66,8 @@ live2d_settings['waifuDraggableRevert'] = false;         // 松开鼠标还原�
 // 其他杂项设置
 live2d_settings['l2dVersion']           = '1.4.2';        // 当前版本
 live2d_settings['l2dVerDate']           = '2018.11.12'; // 版本更新日期
-live2d_settings['homePageUrl']          = '{https://blog.lucas04.xyz/}';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
-live2d_settings['aboutPageUrl']         = '{https://blog.lucas04.xyz/about.html}';   // 关于页地址, '{URL 网址}'
+live2d_settings['homePageUrl']          = '';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
+live2d_settings['aboutPageUrl']         = 'about.html';   // 关于页地址, '{URL 网址}'
 live2d_settings['screenshotCaptureName']= 'live2d.png'; // 看板娘截图文件名，例如 'live2d.png'
 
 /****************************************************************************************************/

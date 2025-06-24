36655014c215bd6e59f3612a5cc16132fdbf88cf
Lucas <lucas04@foxmail.com>
2025-06-24 10:23:25 +0800

    feat: Update project files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+// Nucleo Icons
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+
+import materialKit from "./material-kit";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(materialKit);
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "url";
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});

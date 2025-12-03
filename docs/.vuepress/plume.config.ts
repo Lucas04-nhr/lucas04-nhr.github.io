8657bebdd5c3f5fcb39b535f38909919ded9ef22
Haoran Nie <lucas04@foxmail.com>
2025-12-03 16:43:18 +0800

    feat: Update social links and footer information in VuePress configuration

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -23,7 +23,12 @@ export default defineThemeConfig({
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'github', link: 'https://github.com/Lucas04-nhr' },
+    { icon: 'gitlab', link: 'https://gitlab.com/Lucas04' },
+    { icon: 'twitter', link: 'https://x.com/__Lucas04__' },
+    { icon: 'discord', link: 'https://discord.com/users/1234567890' },
+    { icon: 'bilibili', link: 'https://space.bilibili.com/474539451' },
+    { icon: 'zhihu', link: 'https://www.zhihu.com/people/NHR-zh-cn' },
   ],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
@@ -33,24 +38,24 @@ export default defineThemeConfig({
    * 文章版权信息
    * @see https://theme-plume.vuejs.press/guide/features/copyright/
    */
-  // copyright: true,
+  copyright: true,
 
-  // prevPage: true,   // 是否启用上一页链接
-  // nextPage: true,   // 是否启用下一页链接
-  // createTime: true, // 是否显示文章创建时间
+  prevPage: true,   // 是否启用上一页链接
+  nextPage: true,   // 是否启用下一页链接
+  createTime: true, // 是否显示文章创建时间
 
   /* 站点页脚 */
-  // footer: {
-  //   message: 'Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
-  //   copyright: '',
-  // },
+  footer: {
+    message: '<a target="_blank" href="https://beian.miit.gov.cn/">赣 ICP 备2025066001 号</a> <br> Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
+    copyright: 'All the contents are produced by Lucas under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> unless otherwise noted.',
+  },
 
   /* 过渡动画 @see https://theme-plume.vuejs.press/config/theme/#transition */
-  // transition: {
-  //   page: true,        // 启用 页面间跳转过渡动画
-  //   postList: true,    // 启用 博客文章列表过渡动画
-  //   appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
-  // },
+  transition: {
+    page: true,        // 启用 页面间跳转过渡动画
+    postList: true,    // 启用 博客文章列表过渡动画
+    appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
+  },
 
   locales: {
     '/': {
@@ -79,6 +84,12 @@ export default defineThemeConfig({
        //   title: '',
        //   content: '',
        // },
+
+      footer: {
+        message: '<a target="_blank" href="https://beian.miit.gov.cn/">赣 ICP 备2025066001 号</a> <br> Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
+        copyright: 'All the contents are produced by Lucas under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> unless otherwise noted.',
+      },
+
     },
     '/zh/': {
       /**
@@ -87,10 +98,10 @@ export default defineThemeConfig({
       profile: {
         avatar: 'https://avatars.githubusercontent.com/u/117806313',
         name: 'Lucas',
-        description: 'An undergraduate student studying bioinformatics.',
+        description: '一个学生信的废柴大学生。',
         circle: true,
-        location: 'Wuhan, China',
-        organization: 'Huazhong University of Science and Technology',
+        location: '中国',
+        organization: '华中科技大学',
       },
 
       navbar: zhNavbar,
@@ -106,6 +117,10 @@ export default defineThemeConfig({
        //   title: '',
        //   content: '',
        // },
+      footer: {
+        message: 'ICP 备案号：<a target="_blank" href="https://beian.miit.gov.cn/">赣 ICP 备2025066001 号</a> <br> 由 <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a> 驱动',
+        copyright: '本站所有内容版权均由 Lucas 所有，除特别说明外受 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> 协议保护。',
+      },
     },
   },
 })

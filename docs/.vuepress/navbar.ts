21ee6a86946e040b734a6cec88f4fbb1656141a0
Haoran Nie <lucas04@foxmail.com>
2025-12-03 15:04:55 +0800

    feat: Move to vuepress

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,30 @@
+/**
+ * @see https://theme-plume.vuejs.press/config/navigation/ 查看文档了解配置详情
+ *
+ * Navbar 配置文件，它在 `.vuepress/plume.config.ts` 中被导入。
+ */
+
+import { defineNavbarConfig } from 'vuepress-theme-plume'
+
+export const enNavbar = defineNavbarConfig([
+  { text: 'Home', link: '/' },
+  { text: 'Blog', link: '/blog/' },
+  { text: 'Tags', link: '/blog/tags/' },
+  { text: 'Archives', link: '/blog/archives/' },
+  {
+    text: 'Notes',
+    items: [{ text: 'Demo', link: '/demo/README.md' }]
+  },
+])
+
+export const zhNavbar = defineNavbarConfig([
+  { text: '首页', link: '/zh/' },
+  { text: '博客', link: '/zh/blog/' },
+  { text: '标签', link: '/zh/blog/tags/' },
+  { text: '归档', link: '/zh/blog/archives/' },
+  {
+    text: '笔记',
+    items: [{ text: '示例', link: '/zh/demo/README.md' }]
+  },
+])
+
--- src/utils/markdown.js
+++
@@ -1,9 +0,0 @@
-import MarkdownIt from "markdown-it";
-
-const markdownParser = new MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-});
-
-export default markdownParser;

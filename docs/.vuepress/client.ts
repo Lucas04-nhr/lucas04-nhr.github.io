873600781ca058f8ca9172e57b946573aba2c8eb
Lucas <lucas04@foxmail.com>
2026-03-01 15:34:03 +0800

    chore(dept): Update dependencies

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -32,10 +32,10 @@ export default defineClientConfig({
 
   enhance({ app, router }) {
     // built-in components
-    // app.component('RepoCard', RepoCard)
-    // app.component('NpmBadge', NpmBadge)
-    // app.component('NpmBadgeGroup', NpmBadgeGroup)
-    // app.component('Swiper', Swiper) // you should install `swiper`
+    app.component('RepoCard', RepoCard)
+    app.component('NpmBadge', NpmBadge)
+    app.component('NpmBadgeGroup', NpmBadgeGroup)
+    app.component('Swiper', Swiper) // you should install `swiper`
 
     // your custom components
     app.component("ResponsiveImage", ResponsiveImage);

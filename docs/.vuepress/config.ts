dfa2ebe9090ecfeee97328297536b228e44ec207
Lucas <lucas04@foxmail.com>
2026-03-13 14:09:01 +0800

    chore: Disable contributors display in config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -107,9 +107,7 @@ export default defineUserConfig({
     lastUpdated: {
       formatOptions: { dateStyle: "short", timeStyle: "short" },
     },
-    contributors: {
-      mode: "block", // 显示贡献者名字，设置为 'block' 则显示头像和名字
-    },
+    contributors: false,
     changelog: false,
 
     /**
